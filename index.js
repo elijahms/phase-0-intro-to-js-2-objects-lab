a86d6d7995697c9fc591a6e3f3d580eaa5a4c147
Elijah Silverman <elijahmsilverman@gmail.com>
2021-09-26 23:14:34 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+const employee = {
+    name: 'elijah',
+    streetAdress: "Bronx, NY",
+};
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    
+    const newthing = {
+        ...employee,
+        [key] : value,
+    };
+    return newthing;
+};
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    
+    employee[key] = value;
+    
+    return employee;
+};
+function deleteFromEmployeeByKey(employee, key) {
+    
+    const newthing = {...employee};
+    
+    delete newthing[key];
+
+    return newthing
+};
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    
+    delete employee[key];
+
+    return employee;
+};
